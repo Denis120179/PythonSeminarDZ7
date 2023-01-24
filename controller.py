f5d1a77ea0359b3e00453b2b77440466a2435254
Denis <Denis120179@yandex.by>
2023-01-24 14:07:15 +0300

    777

--- controller.py
+++ controller.py
@@ -1,20 +1,21 @@
 import view, import_data, export_data
 
 def start():
-    action = view.menu_action()
-    if action == 1:
-        import_data.add_data()
-    #if action == 4:
-        #export_data.print_name()
-    if action == 5:
-        export_data.print_data()
-    if action == 6:
-        export_data.print_name()
-    if action == 7:
-        export_data.sort_name()
-    if action == 8:
-        export_data.sort_id()
-        
+    while True:
+        action = view.menu_action()
+        if action == 1:
+            import_data.add_data()
+        if action == 4:
+            break
+        if action == 5:
+            export_data.print_data()
+        if action == 6:
+            export_data.print_name()
+        if action == 7:
+            export_data.sort_name()
+        if action == 8:
+            export_data.sort_id()
+
     
     
     
